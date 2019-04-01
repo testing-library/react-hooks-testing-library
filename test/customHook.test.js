cc7e101317295f01b54f60c2bcbf89892fd5ad2c
Michael Peyper <michael.peyper@ioof.com.au>
2019-04-01 13:06:44 +1100

    Simplified usage example in README

--- test/customHook.test.js
+++ test/customHook.test.js
@@ -2,44 +2,30 @@ import { useState, useCallback } from 'react'
 import { renderHook, cleanup, act } from 'src'
 
 describe('custom hook tests', () => {
-  function useCounter(initialCount = 0) {
-    const [count, setCount] = useState(initialCount)
+  function useCounter() {
+    const [count, setCount] = useState(0)
 
-    const incrementBy = useCallback((n) => setCount(count + n), [count])
-    const decrementBy = useCallback((n) => setCount(count - n), [count])
+    const increment = useCallback(() => setCount(count + 1), [count])
+    const decrement = useCallback(() => setCount(count - 1), [count])
 
-    return { count, incrementBy, decrementBy }
+    return { count, increment, decrement }
   }
 
   afterEach(cleanup)
 
-  test('should create counter', () => {
-    const { result } = renderHook(() => useCounter())
-
-    expect(result.current.count).toBe(0)
-  })
-
   test('should increment counter', () => {
     const { result } = renderHook(() => useCounter())
 
-    act(() => result.current.incrementBy(1))
+    act(() => result.current.increment())
 
     expect(result.current.count).toBe(1)
-
-    act(() => result.current.incrementBy(2))
-
-    expect(result.current.count).toBe(3)
   })
 
   test('should decrement counter', () => {
     const { result } = renderHook(() => useCounter())
 
-    act(() => result.current.decrementBy(1))
+    act(() => result.current.decrement())
 
     expect(result.current.count).toBe(-1)
-
-    act(() => result.current.decrementBy(2))
-
-    expect(result.current.count).toBe(-3)
   })
 })
