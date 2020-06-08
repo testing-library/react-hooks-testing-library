0b747ac1added9086512118e04d8613d519870cf
Michael Peyper <michael.peyper@ioof.com.au>
2020-06-08 22:02:52 +1000

    Add root level imports for each renderer

---
+++ native/index.js
@@ -0,0 +1,2 @@
+// makes it so people can import from '@testing-library/react-hooks/native'
+module.exports = require('../lib/native')
---
+++ server/pure.js
@@ -0,0 +1,2 @@
+// makes it so people can import from '@testing-library/react-hooks/server/pure'
+module.exports = require('../lib/server/pure')
