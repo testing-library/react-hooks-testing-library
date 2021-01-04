50cdd55a08ed377a47b881d40a1844068a23480b
Michael Peyper <mpeyper7@gmail.com>
2021-01-04 22:55:19 +1100

    feat: added render utility support for custom renderers
    BREAKING CHANGE: result of createCustomRenderer is no longer an object

--- src/custom/pure.ts
+++ src/custom/pure.ts
@@ -1,8 +1,6 @@
-import { createRenderHook, cleanup, addCleanup, removeCleanup } from '../core'
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const createCustomRenderer = (createRenderer: any) => ({
-  renderHook: createRenderHook(createRenderer)
-})
-
-export { createCustomRenderer, cleanup, addCleanup, removeCleanup }
+export {
+  createRenderHook as createCustomRenderer,
+  cleanup,
+  addCleanup,
+  removeCleanup
+} from '../core'
