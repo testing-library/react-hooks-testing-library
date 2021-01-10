62f05ba7e1ab06a872c7f4ccf70b13472a5b2ffb
Michael Peyper <mpeyper7@gmail.com>
2021-01-10 20:46:15 +1100

    chore(config): update outdated configs after test changes

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line
-const { jest: jestConfig } = require('kcd-scripts/config')
-
-module.exports = Object.assign(jestConfig, {
-  roots: ['<rootDir>/src'],
-  testMatch: ['<rootDir>/**/__tests__/*.(ts|tsx|js)']
-})
