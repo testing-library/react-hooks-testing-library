b8b9c8b7054196813ee05775c7adfb8d4c79e1aa
Josh Ellis <joshua.ellis18@gmail.com>
2021-01-15 14:46:47 +0000

    refactor: reduce eslint-warnings & type changes

--- src/helpers/act.ts
+++ src/helpers/act.ts
@@ -1,21 +1,19 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Act } from '../types/react'
 
 import { suppressErrorOutput } from './console'
 
+import { isPromise } from './promises'
+
 function createActWrapper(baseAct: Act) {
-  const act: Act = async (callback: () => any) => {
+  const act: Act = async (callback: () => unknown) => {
     const restoreOutput = suppressErrorOutput()
     try {
       let awaitRequired = false
-      const actResult = (baseAct(() => {
+      const actResult = baseAct(() => {
         const callbackResult = callback()
-        awaitRequired = callbackResult !== undefined && !!callbackResult.then
-        return callbackResult
-      }) as any) as PromiseLike<undefined>
-
+        awaitRequired = isPromise(callbackResult)
+        return callbackResult as Promise<void>
+      })
       return awaitRequired ? await actResult : undefined
     } finally {
       restoreOutput()
