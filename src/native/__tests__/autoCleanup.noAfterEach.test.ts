94371170e1efc2edc7b00570593d6f9c27e14249
Michael Peyper <mpeyper7@gmail.com>
2021-01-19 15:56:50 +1100

    chore: added additional tests to ensure pure imports don't add side effects

--- src/native/__tests__/autoCleanup.noAfterEach.test.ts
+++ src/native/__tests__/autoCleanup.noAfterEach.test.ts
@@ -6,7 +6,7 @@ import { ReactHooksRenderer } from '../../types/react'
 // then we DON'T auto-wire up the afterEach for folks
 describe('skip auto cleanup (no afterEach) tests', () => {
   let cleanupCalled = false
-  let renderHook: (arg0: () => void) => void
+  let renderHook: ReactHooksRenderer['renderHook']
 
   beforeAll(() => {
     // @ts-expect-error Turning off AfterEach -- ignore Jest LifeCycle Type
--- src/server/__tests__/autoCleanup.disabled.test.ts
+++ src/server/__tests__/autoCleanup.disabled.test.ts
@@ -6,7 +6,7 @@ import { ReactHooksRenderer } from '../../types/react'
 // then we DON'T auto-wire up the afterEach for folks
 describe('skip auto cleanup (disabled) tests', () => {
   let cleanupCalled = false
-  let renderHook: (arg0: () => void) => void
+  let renderHook: ReactHooksRenderer['renderHook']
 
   beforeAll(() => {
     process.env.RHTL_SKIP_AUTO_CLEANUP = 'true'
