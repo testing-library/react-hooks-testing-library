680021e644be8a75c74f62ca6edb17290db3be36
Michael Peyper <michael.peyper@ailo.io>
2021-03-01 22:04:04 +1100

    test: add tests for `suppressErrorOutput` export

--- src/dom/__tests__/errorSuppression.pure.test.ts
+++ src/dom/__tests__/errorSuppression.pure.test.ts
@@ -1,15 +1,29 @@
+import { ReactHooksRenderer } from '../../types/react'
+
 // This verifies that if pure imports are used
 // then we DON'T auto-wire up the afterEach for folks
 describe('error output suppression (pure) tests', () => {
   const originalConsoleError = console.error
 
+  let suppressErrorOutput!: ReactHooksRenderer['suppressErrorOutput']
+
   beforeAll(() => {
-    require('../pure')
+    suppressErrorOutput = (require('../pure') as ReactHooksRenderer).suppressErrorOutput
   })
 
   test('should not patch console.error', () => {
     expect(console.error).toBe(originalConsoleError)
   })
-})
 
-export {}
+  test('should manually patch console.error', () => {
+    const restore = suppressErrorOutput()
+
+    try {
+      expect(console.error).not.toBe(originalConsoleError)
+    } finally {
+      restore()
+    }
+
+    expect(console.error).toBe(originalConsoleError)
+  })
+})
