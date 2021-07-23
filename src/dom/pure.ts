1f0bf1db4e6b61e7fae79822aa341784478c047a
Rob Snow <rsnow@adobe.com>
2021-07-22 18:12:23 -0700

    Will definitely need something like this in order to support 17 and 18 at once

--- src/dom/pure.ts
+++ src/dom/pure.ts
@@ -6,6 +6,8 @@ import { RendererProps, RendererOptions } from '../types/react'
 import { createRenderHook } from '../core'
 import { createTestHarness } from '../helpers/createTestHarness'
 
+const isReactConcurrent = !!ReactDOM.createRoot;
+
 function createDomRenderer<TProps, TResult>(
   rendererProps: RendererProps<TProps, TResult>,
   { wrapper }: RendererOptions<TProps>
@@ -13,24 +15,46 @@ function createDomRenderer<TProps, TResult>(
   const container = document.createElement('div')
   const testHarness = createTestHarness(rendererProps, wrapper)
   // @ts-ignore
-  const root = ReactDOM.createRoot(container)
-  return {
-    render(props?: TProps) {
-      act(() => {
-        root.render(testHarness(props))
-      })
-    },
-    rerender(props?: TProps) {
-      act(() => {
-        root.render(testHarness(props))
-      })
-    },
-    unmount() {
-      act(() => {
-        root.unmount()
-      })
-    },
-    act
+  if (isReactConcurrent) {
+    // @ts-ignore
+    const root = ReactDOM.createRoot(container)
+    return {
+      render(props?: TProps) {
+        act(() => {
+          root.render(testHarness(props))
+        })
+      },
+      rerender(props?: TProps) {
+        act(() => {
+          root.render(testHarness(props))
+        })
+      },
+      unmount() {
+        act(() => {
+          root.unmount()
+        })
+      },
+      act
+    }
+  } else {
+    return {
+      render(props?: TProps) {
+        act(() => {
+          ReactDOM.render(testHarness(props), container)
+        })
+      },
+      rerender(props?: TProps) {
+        act(() => {
+          ReactDOM.render(testHarness(props), container)
+        })
+      },
+      unmount() {
+        act(() => {
+          ReactDOM.unmountComponentAtNode(container)
+        })
+      },
+      act
+    }
   }
 }
 
